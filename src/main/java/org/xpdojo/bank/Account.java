4c808685eb0a143bfdc6ebc9b60bb3638f07d9f3
Wudong Liu <wudong.liu@gmail.com>
2020-07-16 14:59:59 +0100

    add additional tests

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -8,12 +8,13 @@ public class Account {
         this.balance +=amount;
     }
 
-    public void withdraw(double balance) {
-
+    public void withdraw(double amount) {
+        this.balance -=amount;
     }
 
     public void transfer(Account account, double balance) {
-
+        withdraw(balance);
+        account.deposit(balance);
     }
 
     public double balance() {
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -13,4 +13,28 @@ public class AccountTest {
         account.deposit(10);
         Assertions.assertThat(account.balance()).isEqualTo(10);
     }
+
+    @Test
+    public void withdrawAnAmountToDecreaseTheBalance() {
+        Account account = new Account();
+        account.deposit(10);
+        account.withdraw(5);
+        Assertions.assertThat(account.balance()).isEqualTo(5);
+    }
+
+    @Test
+    public void testTransferBetweenTest() {
+        Account account = new Account();
+        account.deposit(10);
+
+        Account account2 = new Account();
+        account2.deposit(10);
+
+        account.transfer(account2, 10);
+
+        Assertions.assertThat(account.balance()).isEqualTo(0);
+        Assertions.assertThat(account2.balance()).isEqualTo(20);
+    }
+
+
 }
